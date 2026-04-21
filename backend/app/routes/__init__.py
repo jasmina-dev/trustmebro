75eccde995a37f70a97d5b06edffe02f063b2a0b
mazzag <mazzag@lafayette.edu>
2026-04-21 13:30:48 -0400

    update for server routing

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -3,8 +3,4 @@ from flask import Blueprint
 bp = Blueprint("api", __name__)
 main_bp = Blueprint("main", __name__)
 
-@main_bp.route("/")
-def home():
-	return "Backend is alive"
-
 from . import markets, chatbot
